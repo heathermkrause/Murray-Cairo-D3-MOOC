7b7252dc81f040e84f466468689b9f4ce63255c8
heathermkrause <heather@idatassist.com>
2015-03-30 14:12:07 -0400

    Adding R code for data manipulation

---
+++ Food Security Data.R
@@ -0,0 +1,9 @@
+##Food Security Data Visualization
+
+rm(list = ls())
+library(nlme)
+library(lattice)
+library(car)
+library(spida)
+
+data <- read.csv("/Users/heatherkrause/Documents/Murray-Cairo-D3-MOOC/FoodDataFinal.csv")
